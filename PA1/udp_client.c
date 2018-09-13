0837e0247fe705083efee8ef48af3713e654ebe5
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-09-12 23:10:30 -0600

    Update udp_client.c

--- PA1/udp_client.c
+++ PA1/udp_client.c
@@ -14,6 +14,14 @@
 
 #define MAXBUFSIZE 100
 
+typedef struct message
+{
+	int status;
+	int sequence;
+	char data[100];
+}msg;
+
+
 int main (int argc, char * argv[])
 {
 
@@ -23,8 +31,10 @@ int main (int argc, char * argv[])
 	long int a = 0;
 	struct sockaddr_in remote;              //"Internet socket address structure"
 	int buff_size = 100;
-  	int file_size;
+  int file_size;
 	char data_buffer[1024*1024*4];
+
+	msg msg_struct;
 
 	if (argc < 3)
 	{
@@ -70,14 +80,13 @@ int main (int argc, char * argv[])
 	bzero(buffer,sizeof(buffer));
 
 
-	if ((strcmp(buffer, "ls") == 0)|(strncmp(command, "get", 3) == 0))
+	if ((strcmp(command, "ls") == 0)|(strncmp(command, "get", 3) == 0))
 	{
 		nbytes = recvfrom(sock, (char *)buffer, MAXBUFSIZE,
 									MSG_WAITALL, (struct sockaddr *) &from_addr,
 									&addr_length);
 
 		file_size = atoi(buffer);
-		printf("file_size %s\n", buffer);
 
 		FILE *fp;
 		fp = fopen("server_received_file","w+");
@@ -92,7 +101,7 @@ int main (int argc, char * argv[])
 			nbytes = recvfrom(sock, (char *)buffer, MAXBUFSIZE,
                 		0, (struct sockaddr *)&from_addr,
                 		&addr_length);
-			printf("received bytes %i\n",nbytes);
+			printf("received nbytes %i\n",nbytes);
 			if(fwrite(buffer,1,nbytes,fp)<0)
     			{
       				perror("error writting file");
@@ -124,10 +133,11 @@ int main (int argc, char * argv[])
 		nbytes = sendto(sock, (const char *)data_buffer, strlen(data_buffer),
 		        	MSG_CONFIRM, (const struct sockaddr *) &remote,
 		            	sizeof(remote));
-		printf("nbytes %i\n", nbytes);
+		printf("file size %s\n", data_buffer);
 
 		while(a <= file_size_put)
 		{
+			sleep(0.1);
 			a += buff_size;
 			int num = a - file_size_put;
 			if (num > 0)
@@ -135,7 +145,7 @@ int main (int argc, char * argv[])
 				printf("In while loop...%ld\n",(a-file_size_put));
 				buff_size = file_size_put - (a-buff_size);
 			}
-			printf("buff size %i\n", buff_size);
+
 			fr = fread(data_buffer,buff_size,1,fd);
 			if (fr<0)
     			{
@@ -145,11 +155,16 @@ int main (int argc, char * argv[])
 			nbytes = sendto(sock, (const char *)data_buffer, buff_size,
         			MSG_CONFIRM, (const struct sockaddr *) &remote,
             			sizeof(remote));
-
-			printf(" send bytes %i\n",nbytes);
 		}
 	}
-	else printf("Server says\n%s", buffer);
+	else
+	{
+		nbytes = recvfrom(sock, (char *)buffer, MAXBUFSIZE,
+									MSG_WAITALL, (struct sockaddr *) &from_addr,
+									&addr_length);
+									printf("reecived nbytes %i\n", nbytes);
+		printf("Server says %s\n", buffer);
+	}
 
 	close(sock);
 
