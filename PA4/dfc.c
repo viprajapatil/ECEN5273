ae5054e7b9b40f0e5c2da8a6b9d3ffe215212fe6
viprajapatil <31683311+viprajapatil@users.noreply.github.com>
2018-12-16 22:30:25 -0700

    Update dfc.c

--- PA4/dfc.c
+++ PA4/dfc.c
@@ -26,6 +26,7 @@ char subfolder[20];
 char conf[50];
 int index_value[8];
 int i = 0;
+int status[4];
 
 void get_conf_info()
 {
@@ -86,19 +87,18 @@ void connect_to_servers()
 	      if (server_connect < 0)
 	      {
 			       perror("Failed to connect to host");
+             status[i] = server_connect;
 			       close(sockfd[i]);
 			       return;
 	      }
 
         char authen[100];
         sprintf(authen,"%s %s %s",username,password,subfolder);
-        printf("authen len->%d\n", strlen(authen));
         int nbytes = send(sockfd[i], authen, strlen(authen), 0);
         if (nbytes < 0){
           printf("nbytes-> %d\n", nbytes);
         }
         else printf("nbytes-> %d\n", nbytes);
-
 
         printf("Sucessful connection for port %d \n", dfs_server_port[i]);
 	      //setsockopt (sockfd[i], SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,sizeof(timeout);
@@ -158,6 +158,9 @@ Put a file into 4 dfs servers
 */
 void put_file(char *filename)
 {
+  char d;
+  for(int i=0;i<strlen(password)-1;i++)
+      d = password[i]^password[i+1];
   FILE *f = fopen(filename, "rb");
   printf("filename-> %s\n", filename);
   if(f==NULL)
@@ -181,21 +184,29 @@ void put_file(char *filename)
     char buffer0[send_size];
     memset(buffer0,'\0',send_size);
     fread(buffer0,send_size,1,f);
+    for(int i=0; i<strlen(buffer0);i++)
+        buffer0[i] = buffer0[i]^1;
 
     fseek(f,send_size,SEEK_SET); /*set file pointer to start of file*/
     char buffer1[send_size];
     memset(buffer1,'\0',send_size);
     fread(buffer1,send_size,1,f);
+    for(int i=0; i<strlen(buffer1);i++)
+        buffer1[i] = buffer1[i]^1;
 
     fseek(f,send_size*2,SEEK_SET); /*set file pointer to start of file*/
     char buffer2[send_size];
     memset(buffer2,'\0',send_size);
     fread(buffer2,send_size,1,f);
+    for(int i=0; i<strlen(buffer2);i++)
+        buffer2[i] = buffer2[i]^1;
 
     fseek(f,send_size*3,SEEK_SET); /*set file pointer to start of file*/
     char buffer3[last_size];
     memset(buffer3,'\0',last_size);
     fread(buffer3,last_size,1,f);
+    for(int i=0; i<strlen(buffer3);i++)
+        buffer3[i] = buffer3[i]^1;
     int c =0;
     // send files to dfs servers
   /*  for (int i=0; i<4; i++)
@@ -203,46 +214,50 @@ void put_file(char *filename)
 
     }*/
     int nbytes;
-    char filename_ext[100];
+    char filename_ext[20];
     char buffer_num[100];
     int index = 0;
     int count = 1;
     for(int i=0; i<4; i++)
     {
+        if(status[i] < 0)
+        {
+          index = index+2;
+          continue;
+        }
+
         while(count<3)
         {
             bzero(filename_ext,sizeof(filename_ext));
             sprintf(filename_ext,".%s.%d",filename,index_value[index]);
-            printf("ext-> %s\n",filename_ext);
-            char buffer[5];
-            recv(sockfd[i],buffer,sizeof(buffer),0);
+            filename_ext[strlen(filename_ext)] = '\0';
+            char sample[5];
+            bzero(sample,sizeof(sample));
+            nbytes= recv(sockfd[i],sample,sizeof(sample),0);
             nbytes = send(sockfd[i], filename_ext, strlen(filename_ext), 0);
             //sleep(2);
-            //printf("nbytes ext->%d\n", nbytes);
+            recv(sockfd[i],sample,sizeof(sample),0);
             if(index_value[index] == 1)
-            {   printf("entered %d loop...\n",index_value[index]);
+            {
                 nbytes = send(sockfd[i], buffer0, send_size, 0);
             }
             else if (index_value[index] == 2)
             {
-              printf("entered %d loop...\n",index_value[index]);
               nbytes = send(sockfd[i], buffer1, send_size, 0);
             }
             else if (index_value[index] == 3)
             {
-                printf("entered %d loop...\n",index_value[index]);
                 nbytes = send(sockfd[i], buffer2, send_size, 0);
             }
-            else
+            else if (index_value[index] == 4)
             {
-                printf("entered %d loop...\n",index_value[index]);
                 nbytes = send(sockfd[i], buffer3, last_size, 0);
             }
+            printf("\nnbytes->%d\n",nbytes);
+            printf("server->%d, file->%d\n\n", i,index_value[index]);
             index++;
-            printf("nbytes content->%d\n", nbytes);
-
             count++;
-            printf("\nserver->%d, file->%d\n", i,index_value[index]);
+
 
         }
         count = 1;
@@ -252,79 +267,175 @@ void put_file(char *filename)
 }
 
 
-char* merge_file(char file[])
-{
-    char *s1;
-    char *s2;
-    char *file_name;
-    file_name = strtok(file, ".");
-    s2 = strtok(NULL,".");
-    s1 = strtok(NULL,".");
-    *(file_name + strlen(file_name)) = '.';
-    //*(file_name + strlen(file_name)-2) = '\0';
-    printf("*******%s********\n", file_name);
-    return file_name;
-}
-
-
-
-
 void get_file()
-{   int nbytes, h=0;
+{
+  char d;
+  for(int i=0;i<strlen(password)-1;i++)
+      d = password[i]^password[i+1];
+  int nbytes, h=0;
     long long int n;
     char ser[10] = "random";
     char filename_get[100];
-    char *file_name;
+    char file_name[100];
     char p[4][100];
-    char server_data[1024*1024*6];
+    char server_data[1000000];
+    int c = 0;
     for(int i=0; i<4; i++)
     {
-
+      if(status[i] < 0)
+          continue;
         for(int j=0; j<2; j++)
         {
           bzero(filename_get,sizeof(filename_get));
           bzero(server_data,sizeof(server_data));
-          send(sockfd[i], "hello", 5, 0); //for sync
+          sleep(1);
+          nbytes = send(sockfd[i], "hello", 5, 0); //for sync
           nbytes = recv(sockfd[i], filename_get, sizeof(filename_get), 0);
-          printf("file rec->%s\n",filename_get);
+          printf("\nfileext->%s ,n->%d\n", filename_get,nbytes);
+          if (strcmp(filename_get,"ERROR") == 0)
+              continue;
+          sprintf(file_name,"%s",filename_get);
+          //printf("filename->%s\n\n", file_name);
           FILE *f = fopen(filename_get,"wb");
-          nbytes = 1;
-          // while(nbytes>0)
-          // {
+
               send(sockfd[i], "hello", 5, 0); //for sync
+              nbytes = 1;
+              while(nbytes > 0)
+              {
               nbytes = recv(sockfd[i], server_data, sizeof(server_data), 0);
-//              if (nbytes == 0)
-//                  break;
+              printf("nbytes->%d\n", nbytes);
+              for(int i=0; i<strlen(server_data);i++)
+                  server_data[i] = server_data[i]^1;
               if (nbytes > 0)
               {
               fwrite(server_data,1,nbytes,f);
               }
-//              if(n < sizeof(server_data))
-//                break;
-//          }
+              if(nbytes < sizeof(server_data))
+              {
+                break;
+              }
+            }
           fclose(f);
-
+          c++;
         }
-        file_name = merge_file(filename_get);
+
     }
+
     char file_with_ext[100];
+    file_name[strlen(file_name)-2] = '\0';
     for (int i=1; i<=4; i++)
     {
       bzero(file_with_ext,sizeof(file_with_ext));
-      sprintf(file_with_ext,".%s.%d",file_name,i);
-      printf("file->%s\n", file_with_ext);
+      //printf("filename->%s\n",file_name);
+      sprintf(file_with_ext,"%s.%d",file_name,i);
+      //printf("file->%s\n", file_with_ext);
       strcpy(p[i-1],file_with_ext);
     }
     char cmd_merge[200];
-    sprintf(cmd_merge,"cat %s %s %s %s > %s",p[0],p[1],p[2],p[3],file_name);
+    sprintf(cmd_merge,"cat %s %s %s %s > %s",p[0],p[1],p[2],p[3],file_name+1);
+    printf("merge->%s\n", cmd_merge);
     system(cmd_merge);
 
 }
 
+
+void list_func()
+{
+      char server_data[1000];
+      int c =0;
+      char line[100];
+      char file_name_list[100];
+      char file[100];
+      bzero(file,sizeof(file));
+      bzero(server_data,sizeof(server_data));
+      for(int i=0;i<4;i++)
+      {
+        if(status[i] < 0)
+            continue;
+
+          bzero(file_name_list,sizeof(file_name_list));
+          sprintf(file_name_list,"list%d.txt",i);
+          int nbytes = recv(sockfd[i], server_data, sizeof(server_data), 0);
+          FILE *f = fopen(file_name_list,"w");
+          fwrite(server_data,1,nbytes,f);
+          fclose(f);
+          system("sort -u list* > list.txt");
+      }
+      FILE *f = fopen("list.txt","r");
+      if(f==NULL)
+          perror("fopen error");
+      while(fgets(line,sizeof(line),f) != NULL)
+      {
+          if(strstr(line,".1") != NULL)
+          {
+            printf("%s\n", line);
+            char *fl = line;
+            *(fl+strlen(fl)-2)='\0';
+
+            fgets(line,sizeof(line),f);
+            if (strstr(line,".2") == NULL){
+                printf("%s2 missing\n",fl);
+              }
+            else {printf("%s\n", line);
+            fgets(line,sizeof(line),f);
+          }
+
+            if (strstr(line,".3") == NULL){
+                printf("%s3 missing\n",fl);}
+            else {printf("%s\n", line);
+            fgets(line,sizeof(line),f);
+            }
+
+            if (strstr(line,".4") == NULL){
+                printf("%s4 missing\n",fl);}
+            else printf("%s\n", line);
+          }
+          else if(strstr(line,".2") != NULL)
+          {
+            char *fl = line;
+            *(fl+strlen(fl)-2)='\0';
+            printf("%s1 missing\n", fl);
+            printf("%s\n", line);
+
+            fgets(line,sizeof(line),f);
+            if (strstr(line,".3") == NULL)
+                printf("%s3 missing\n",fl);
+            else{ printf("%s\n", line);
+            fgets(line,sizeof(line),f);}
+
+            if (strstr(line,".4") == NULL)
+                printf("%s4 missing\n",fl);
+            else printf("%s\n", line);
+          }
+          else if(strstr(line,".3") != NULL)
+          {
+            char *fl = line;
+            *(fl+strlen(fl)-2)='\0';
+            printf("%s1 missing\n", fl);
+            printf("%s2 missing\n", fl);
+            printf("%s\n", line);
+            fgets(line,sizeof(line),f);
+            if (strstr(line,".4") == NULL)
+                printf("%s4 missing\n",fl);
+            else printf("%s\n", line);
+          }
+          else if(strstr(line,".4") != NULL)
+          {
+            char *fl = line;
+            *(fl+strlen(fl)-2)='\0';
+            printf("%s1 missing\n", fl);
+            printf("%s2 missing\n", fl);
+            printf("%s3 missing\n", fl);
+            printf("%s\n", line);
+          }
+      }
+}
+
+
 int main(int argc, char **argv)
 {
     char command[100];
-    char *filename;
+    char *filename, *sub_folder;
     if (argc < 3)
     {
         printf("Less args <dfc config file> <subfolder>");
@@ -360,13 +471,13 @@ int main(int argc, char **argv)
 	      if (server_connect < 0)
 	      {
 			       perror("Failed to connect to host");
+             status[i] = server_connect;
 			       close(sockfd[i]);
 			       //return;
 	      }
 
         char authen[100];
         sprintf(authen,"%s %s %s",username,password,subfolder);
-        printf("authen len->%d\n", strlen(authen));
         int nbytes = send(sockfd[i], authen, strlen(authen), 0);
         if (nbytes < 0){
           printf("nbytes-> %d\n", nbytes);
@@ -384,7 +495,8 @@ int main(int argc, char **argv)
     scanf("%[^\t\n]s", command);
     getchar();
     for (int i=0; i<4; i++)
-    {
+    { if (status[i] < 0)
+          continue;
       int nbytes = send(sockfd[i], command, strlen(command), 0);
       if (nbytes < 0){
         printf("nbytes-> %d\n", nbytes);
@@ -393,20 +505,26 @@ int main(int argc, char **argv)
 
     if (strstr(command,"get") != NULL)
     {
-      filename = strstr(command, " ");
-      filename++;
+      filename = strtok(command," ");
+      filename = strtok(NULL," ");
+      sub_folder = strtok(NULL, " ");
+      //filename = strstr(command, " ");
+      //filename++;
       get_file();
     }
     else if (strstr(command,"put") != NULL)
     {
-        filename = strstr(command, " ");
-        filename++;
+      filename = strtok(command," ");
+      filename = strtok(NULL," ");
+      sub_folder = strtok(NULL, " ");
+        //filename = strstr(command, " ");
+        //filename++;
         put_file(filename);
     }
     else if (strstr(command, "list") != NULL)
     {
         //call list function
-        printf("list called!/n\n");
+        list_func();
     }
     else printf("Entered wrong command, enter -> list, get or put\n");
 
